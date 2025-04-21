ed80ab50a4a7576e47ce3da5a8a9b8d0f7b20e0c
deepakkumarv128@gmail.com <vishwakarmad128@gmail.com>
2025-04-21 23:35:45 +0530

    initial lib setup

---
+++ Sources/Datablit/Datablit.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class Analytics  {
+    public init() {}
+    public func sendMsg() {
+        print("msg send")
+    }
+}
